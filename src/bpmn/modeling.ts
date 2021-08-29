6ccc4638596c4ffd4996a5777fa03c27b4d36704
greedy0110 <greedy0110@gmail.com>
2021-08-29 22:02:09 +0900

    init graph modeling

--- src/bpmn/modeling.ts
+++ src/bpmn/modeling.ts
@@ -1,6 +1,5 @@
 const Convert = require('xml-js');
-
-export function extract_process_from_xml(xml: string): any {
+export function extract_process_from_xml(xml: string): Array<any> {
   const options = {
     compact: true,
     alwaysArray: true,
@@ -9,8 +8,12 @@ export function extract_process_from_xml(xml: string): any {
 
   const js = Convert.xml2js(xml, options);
 
-  return js['bpmn2:definitions'][0]['bpmn2:process'][0];
+  return js['bpmn2:definitions'][0]['bpmn2:process'];
 }
+
+const attr = '_attributes';
+const attrid = 'id';
+const attrname = 'name';
 
 // id 로 요소 찾기
 export function find_element_by_id(js: any, id: string): any {
@@ -18,9 +21,7 @@ export function find_element_by_id(js: any, id: string): any {
     for (let v of value) {
       let cur_id = v['_attributes']['id'];
       if (cur_id == id) {
-        let ans = Object();
-        ans[key] = value;
-        return ans;
+        return value;
       }
     }
   }
@@ -30,9 +31,7 @@ export function find_element_by_id(js: any, id: string): any {
 export function find_element_by_name(js: any, name: string): any {
   for (let [key, value] of Object.entries<Array<any>>(js)) {
     if (key == name) {
-      let ans = Object();
-      ans[key] = value;
-      return ans;
+      return value;
     }
   }
   return null;
@@ -42,6 +41,77 @@ export function find_element_by_name(js: any, name: string): any {
 // console.log(result);
 // console.log(result['bpmn2:sequenceFlow'][0]['_attributes']['id']);
 // console.log(result['bpmn2:exclusiveGateway'][0]['bpmn2:incoming'][0]);
-// console.log(find_element_by_id(result, 'Flow_0qdz5ko'));
-// console.log(find_element_by_name(result, 'bpmn2:startEvent'));
-// console.log(find_element_by_name(result, 'bpmn2:sequenceFlow'));
+// console.log(find_element_by_id(result[0], 'Flow_0qdz5ko'));
+// console.log(find_element_by_name(result[0], 'bpmn2:startEvent'));
+// console.log(find_element_by_name(result[0], 'bpmn2:sequenceFlow'));
+
+class Node {
+  id: string;
+  name: string;
+
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+}
+
+const attr_flow = 'bpmn2:sequenceFlow';
+const attr_source = 'sourceRef';
+const attr_target = 'targetRef';
+
+let node_cache: { [id: string]: Node } = {};
+
+function getNode(process: any, node_id: string): Node {
+  if (node_id in node_cache) return node_cache[node_id];
+
+  let element = find_element_by_id(process, node_id);
+
+  if (element == null) {
+    throw 'Id undefined' + node_id;
+  }
+  element = element[0];
+  let eid = element[attr][attrid];
+  let name = element[attr][attrname];
+  let node = new Node(eid, name);
+  node_cache[node.id] = node;
+  return node;
+}
+
+function extract_process_graph_from_xml(xml: string): {
+  [id: string]: { [id: string]: Array<Node> };
+} {
+  // Process 별로, 가지고 있음.
+  let ret: { [id: string]: { [id: string]: Array<Node> } } = {};
+  let js = extract_process_from_xml(xml);
+  for (let process of js) {
+    let pid = process[attr][attrid];
+    let name = process[attr][attrname];
+    let pnode = new Node(pid, name);
+    node_cache[pnode.id] = pnode;
+
+    let relations: { [id: string]: Array<Node> } = {};
+
+    // 노드간 모든 관계가 포함되어 있다.
+    let flows = find_element_by_name(process, attr_flow) as Array<any>;
+    for (let flow of flows) {
+      console.log(flow);
+      let source_id = flow[attr][attr_source];
+      let target_id = flow[attr][attr_target];
+
+      let source = getNode(process, source_id);
+      let target = getNode(process, target_id);
+
+      if (!(source.id in relations)) {
+        relations[source.id] = [];
+      }
+      relations[source.id].push(target);
+    }
+
+    ret[pnode.id] = relations;
+  }
+  return ret;
+}
+
+let result = extract_process_graph_from_xml(test_xml);
+
+console.log(result);
