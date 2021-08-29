fd043d56f4f1cff07e5ee7fb55de7bedb89722a7
greedy0110 <greedy0110@gmail.com>
2021-08-29 22:18:04 +0900

    refactor with types
    extract_process_graph_from_xml -> [NodeCache, EdgeCache, Processes]
    
    NodeCache -> 노드의 부가 정보가 다 들어있다.
    EdgeCache -> Flow의 부가 정보가 다 들어있다.
    Processes -> 노드 아이디간 연결 정보가 들어있다.

--- src/bpmn/modeling.ts
+++ src/bpmn/modeling.ts
@@ -1,4 +1,5 @@
 const Convert = require('xml-js');
+
 export function extract_process_from_xml(xml: string): Array<any> {
   const options = {
     compact: true,
@@ -37,14 +38,6 @@ export function find_element_by_name(js: any, name: string): any {
   return null;
 }
 
-// const result = extract_process_from_xml(test_xml);
-// console.log(result);
-// console.log(result['bpmn2:sequenceFlow'][0]['_attributes']['id']);
-// console.log(result['bpmn2:exclusiveGateway'][0]['bpmn2:incoming'][0]);
-// console.log(find_element_by_id(result[0], 'Flow_0qdz5ko'));
-// console.log(find_element_by_name(result[0], 'bpmn2:startEvent'));
-// console.log(find_element_by_name(result[0], 'bpmn2:sequenceFlow'));
-
 class Node {
   id: string;
   name: string;
@@ -55,33 +48,71 @@ class Node {
   }
 }
 
+class Edge {
+  id: string;
+  source: string;
+  target: string;
+
+  constructor(id: string, source: string, target: string) {
+    this.id = id;
+    this.source = source;
+    this.target = target;
+  }
+}
+
+class CompactNode {
+  nodeId: string;
+  edgeId: string;
+
+  constructor(nodeId: string, edgeId: string) {
+    this.nodeId = nodeId;
+    this.edgeId = edgeId;
+  }
+}
+
 const attr_flow = 'bpmn2:sequenceFlow';
 const attr_source = 'sourceRef';
 const attr_target = 'targetRef';
 
-let node_cache: { [id: string]: Node } = {};
+type EdgeCache = {
+  [id: string]: Edge;
+};
 
-function getNode(process: any, node_id: string): Node {
-  if (node_id in node_cache) return node_cache[node_id];
+type NodeCache = {
+  [id: string]: Node;
+};
 
-  let element = find_element_by_id(process, node_id);
+type Graph = {
+  [id: string]: Array<CompactNode>;
+};
 
-  if (element == null) {
-    throw 'Id undefined' + node_id;
+type Processes = {
+  [id: string]: Graph;
+};
+
+function extract_process_graph_from_xml(
+  xml: string
+): [NodeCache, EdgeCache, Processes] {
+  let node_cache: NodeCache = {};
+  let edge_cache: EdgeCache = {};
+
+  function getNode(process: any, node_id: string): Node {
+    if (node_id in node_cache) return node_cache[node_id];
+
+    let element = find_element_by_id(process, node_id);
+
+    if (element == null) {
+      throw 'Id undefined' + node_id;
+    }
+    element = element[0];
+    let eid = element[attr][attrid];
+    let name = element[attr][attrname];
+    let node = new Node(eid, name);
+    node_cache[node.id] = node;
+    return node;
   }
-  element = element[0];
-  let eid = element[attr][attrid];
-  let name = element[attr][attrname];
-  let node = new Node(eid, name);
-  node_cache[node.id] = node;
-  return node;
-}
 
-function extract_process_graph_from_xml(xml: string): {
-  [id: string]: { [id: string]: Array<Node> };
-} {
-  // Process 별로, 가지고 있음.
-  let ret: { [id: string]: { [id: string]: Array<Node> } } = {};
+  let processes: Processes = {};
   let js = extract_process_from_xml(xml);
   for (let process of js) {
     let pid = process[attr][attrid];
@@ -89,14 +120,16 @@ function extract_process_graph_from_xml(xml: string): {
     let pnode = new Node(pid, name);
     node_cache[pnode.id] = pnode;
 
-    let relations: { [id: string]: Array<Node> } = {};
+    let relations: Graph = {};
 
     // 노드간 모든 관계가 포함되어 있다.
     let flows = find_element_by_name(process, attr_flow) as Array<any>;
     for (let flow of flows) {
       console.log(flow);
+      let flow_id = flow[attr][attrid];
       let source_id = flow[attr][attr_source];
       let target_id = flow[attr][attr_target];
+      edge_cache[flow_id] = new Edge(flow_id, source_id, target_id);
 
       let source = getNode(process, source_id);
       let target = getNode(process, target_id);
@@ -104,14 +137,11 @@ function extract_process_graph_from_xml(xml: string): {
       if (!(source.id in relations)) {
         relations[source.id] = [];
       }
-      relations[source.id].push(target);
+
+      relations[source.id].push(new CompactNode(target.id, flow_id));
     }
 
-    ret[pnode.id] = relations;
+    processes[pnode.id] = relations;
   }
-  return ret;
+  return [node_cache, edge_cache, processes];
 }
-
-let result = extract_process_graph_from_xml(test_xml);
-
-console.log(result);
